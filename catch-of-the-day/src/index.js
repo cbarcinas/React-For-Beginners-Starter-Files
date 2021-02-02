64241c361aab455bf57340db0d7851bb77f9c14a
Craig Barcinas <cbarcinas@live.com>
2021-02-01 23:00:46 -0800

    added components, no dynamic props

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+//ICE - import component export
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+
+import "./css/style.css";
+
+render(<App />, document.querySelector("#main")); //takes 2 things, jsx and mounting point
